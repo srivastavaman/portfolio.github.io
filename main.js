1d0f5c2d4200224a62f889a28cd6cebaba29f472
Aman Srivastav <96619298+srivastavaman@users.noreply.github.com>
2023-04-05 17:47:49 +0530

    Uploding Portfolio file

---
+++ main.js
@@ -0,0 +1,8 @@
+function navtoogle() {
+    var x =document.querySelector('.navitems')
+    if (x.style.display==='grid') {
+            x.style.display='none'
+    } else{
+            x.style.display='grid'
+    }
+}
